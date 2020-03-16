26aede25b24c480fd47b1e89df7f2b8029c91482
Kate Foust <kate.e.foust@gmail.com>
2020-03-16 16:21:07 -0700

    added JAWSDB

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured boolean DEFAULT 0, 
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id) 
 );
+
