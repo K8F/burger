49ef712b97919a40863f9d8a94e9d8db3cc55713
Kate Foust <kate.e.foust@gmail.com>
2020-03-16 16:56:41 -0700

    testing heroku deployment

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,5 @@ CREATE TABLE burgers
 	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured boolean DEFAULT 0, 
-    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id) 
 );
-
